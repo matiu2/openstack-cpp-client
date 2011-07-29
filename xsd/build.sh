07f4f32b98454cb11547c1981ea3ee5780f93053
Matthew Sherborne <msherborne@gmail.com>
2011-07-30 04:58:07 +1000

    Generated code and documentation from xsds

---
+++ xsd/build.sh
@@ -0,0 +1,1 @@
+xsdcxx cxx-tree --generate-polymorphic --polymorphic-type-all --generate-inline --namespace-map http://docs.rackspacecloud.com/servers/api/v1.0=openstack::xml --type-naming ucc --function-naming lcc --hxx-suffix .hpp --cxx-suffix .cpp --ixx-suffix .ipp --generate-doxygen --root-element-all --generate-element-type --generate-element-map xsds/*.xsd
