3425bb38b24aaf19eabf38b9012e4da05c23e072
Matthew Sherborne <msherborne@gmail.com>
2011-07-31 23:39:19 +1000

    made the xml files build.sh be explicitly run by /bin/sh

--- xsd/build.sh
+++ xsd/build.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 xsdcxx cxx-tree --generate-polymorphic --polymorphic-type-all --generate-inline --namespace-map http://docs.rackspacecloud.com/servers/api/v1.0=openstack::xml --type-naming ucc --function-naming lcc --hxx-suffix .hpp --cxx-suffix .cpp --ixx-suffix .ipp --generate-doxygen xsds/*.xsd
