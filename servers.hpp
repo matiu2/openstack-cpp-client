b947cf27a7716474c4753451e2093b6bbe84109c
Matthew Sherborne <msherborne@gmail.com>
2011-07-31 23:37:33 +1000

    Made servers.hpp return the right thing

--- servers.hpp
+++ servers.hpp
@@ -22,6 +22,9 @@
 #include <sstream>
 #include <iostream>
 #include "xsd/server.hpp"
+#ifdef USE_INLINE_CODE
+#include "xsd/server.ipp"
+#endif
 
 namespace openstack {
 
@@ -42,27 +45,38 @@ public:
     /**
     * Downloads and parses something like this:
     * 
-    * <?xml version="1.0" encoding="UTF-8" standalone="yes"?><servers xmlns="http://docs.rackspacecloud.com/servers/api/v1.0"><server id="795837" name="greg.archy"/><server id="796534" name="nater"/><server id="20098970" name="matiu.natty"/><server id="20098971" name="ross.natty"/><server id="20099000" name="ross.natty.2"/><server id="20111370" name="matiu.fedora"/></servers>"
+    * <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+    *  <servers xmlns="http://docs.rackspacecloud.com/servers/api/v1.0">
+    *    <server id="1234" name="admin"/>
+    *    <server id="563" name="stats"/>
+    *    <server id="456546" name="web2"/>
+    *    <server id="2345345" name="web1"/>
+    *    <server id="234345" name="db1"/>
+    *    <server id="34788" name="db2"/>
+    *  </servers>"
+    * 
+    * To use the return value:
+    *   const xml::Servers::ServerSequence oc.servers().list().server();
+    *   for(xml::Servers::ServerConstIterator server=list.begin();server!=list.end();++server)
+    *       std::cout << "ID: " << server->id() << " - " << "Name: " << server->name() << std::endl;
     *
-    * And returns an xml::servers element.
+    * @param details if true, will get all the server details. If false, will get just the IDs and names
+    *
+    * @return A smart pointer to an xml::Servers instance.
     */
-    void list() {
+    std::auto_ptr<xml::Servers> list(bool details=false) {
         // Do the request
         HTTPS::Headers requestHeaders, responseHeaders;
         std::stringstream body;
-        requestHeaders.insert(HTTPS::Header("Host", _hostname));
         requestHeaders.insert(HTTPS::Header("X-Auth-Token", _token));
         requestHeaders.insert(HTTPS::Header("Accept", "application/xml"));
-        connection().request(_pathBase + "/servers", requestHeaders, responseHeaders, body);
-        // Parse the body
+        if (details) 
+            connection().request(_pathBase + "/servers/detail", requestHeaders, responseHeaders, body);
+        else
+            connection().request(_pathBase + "/servers", requestHeaders, responseHeaders, body);
+        // Parse the body and return the object
         body.seekp(std::stringstream::beg); // Go back to the beginning of our stream
-        std::auto_ptr<xml::Servers> servers = xml::servers(body, xml_schema::Flags::dont_validate);
-        // Temporary debugging code
-        std::cout << "Found Servers:" << std::endl;
-        const xml::Servers::ServerSequence list = servers->server();
-        for(xml::Servers::ServerConstIterator server=list.begin();server!=list.end();++server) {
-            std::cout << "ID: " << server->id() << " - " << "Name: " << server->name() << std::endl;
-        }
+        return xml::servers(body, xml_schema::Flags::dont_validate);
     }
 };
 
